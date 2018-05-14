ed4949c09cfaa91e6e77dc359d60007613657b29
Robert Santiago <citizenbobx@yahoo.com>
2018-05-14 11:55:04 -0400

    rewrite step 27 Track.js

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -3,7 +3,7 @@ import './Track.css';
 
 class Track extends React.Component{
   renderAction(){
-    if(isRemoval === true;){
+    if(this.props.isRemoval === true){
       return '-';
     }else{
       return '+';
@@ -16,7 +16,7 @@ class Track extends React.Component{
           <h3>{this.props.track.name}</h3>
           <p>{this.props.track.artist} | {this.props.track.album}</p>
         </div>
-        <a className="Track-action"><!-- + or - will go here --></a>
+        <a className="Track-action">{this.renderAction()}</a>
       </div>
     );
   }
