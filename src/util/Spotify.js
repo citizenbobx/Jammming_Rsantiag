d44d437533d55fc1e66f8c3cfa5db0e94f20a0f9
Robert Santiago <citizenbobx@yahoo.com>
2018-05-23 16:26:26 -0400

    utility folder

---
+++ src/util/Spotify.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+//step 82
+const clientID ="0dc1061f92cb455fa1abf0785e7f4d00";
+
+const redirectURI = "http://localhost:3000/";
+
+//step 77
+const apiToken ='';
+
+//step 76
+const Spotify = {
+  //step 78
+  getAccessToken(){
+
+  }//close getAccessToken method
+
+};//close Spotify object
+
+//step 85 v
+search(){
+  'https://api.spotify.com/v1/search?type=track&q=TERM'
+}//close search method
+
+
+
+//step 90
+method(playlistName, [trackURI, trackURI, trackURI]){
+
+}//close
+
+//step 76 v
+export default Spotify;
