c3281122582a9835ba896667f7219ce0b457db6c
Robert Santiago <citizenbobx@yahoo.com>
2018-05-25 18:24:03 -0400

    Jammming steps 57 and 59

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -83,7 +83,7 @@ addTrack(track) {
 
   //step 57 v
   updatePlaylistName(name){
-    this.setState({playlistName : name});
+    this.setState({playlistName: name});
   }//close updatePlaylistName method
 
   //step 63 v
